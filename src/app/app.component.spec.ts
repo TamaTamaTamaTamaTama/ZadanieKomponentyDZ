5daee23c477410debd5c4ebe1a95b6dfac48b0b2
TamaTamaTamaTamaTama <133633707+TamaTamaTamaTamaTama@users.noreply.github.com>
2024-10-23 19:43:11 +0200

    Wykonanie zadania Dominik Zabielski

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'test4' title`, () => {
+  it(`should have the 'BusinessApp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test4');
+    expect(app.title).toEqual('BusinessApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test4');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BusinessApp');
   });
 });
---
+++ src/app/business-card/business-card.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ShowDateComponent } from '../show-date/show-date.component';
+import { CommonModule } from '@angular/common';
+import { ManageInterestsComponent } from '../manage-interests/manage-interests.component';
+
+@Component({
+  selector: 'app-business-card',
+  standalone: true,
+  imports: [FormsModule,ShowDateComponent,CommonModule, ManageInterestsComponent],
+  templateUrl: './business-card.component.html',
+  styleUrl: './business-card.component.css'
+})
+export class BusinessCardComponent {
+  name: string;
+  surname: string;
+  dateOfBirth: Date;
+  interests: string[];
+  selected:number=-1;
+  originalInterest: string | null = null;
+
+
+  constructor() { 
+    this.name = 'Joanna';
+    this.surname = 'Krupa';
+    this.dateOfBirth = new Date('2000-03-18');
+    this.interests = ['tennis', 'programming', 'photography'];
+  
+  }
+
+
+  selectInterest(which:number):void{
+      this.selected=which;
+      this.originalInterest = this.interests[which];
+  }
+  cancelChange(x: number) {
+    if (this.selected > -1 && this.originalInterest !== null) {
+      this.interests[this.selected] = this.originalInterest;
+    }
+    this.selected = -1; 
+    this.originalInterest = null;  
+  }
+  
+    SaveChange(x: number) {
+      this.selected = -1;  
+      this.originalInterest = null; 
+    }
+
+    
+
+
+}
